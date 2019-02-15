2a5ff26ca3bebb6d199ee6e2e12cacbd7d81d217
Ganesh Mannamal <ganeshmannamal@gmail.com>
2019-02-16 00:10:24 +0530

    fix omitempty issues

--- cloudformation/aws-ecs-service.go
+++ cloudformation/aws-ecs-service.go
@@ -23,7 +23,7 @@ type AWSECSService struct {
 	// DesiredCount AWS CloudFormation Property
 	// Required: false
 	// See: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-ecs-service.html#cfn-ecs-service-desiredcount
-	DesiredCount int `json:"DesiredCount,omitempty"`
+	DesiredCount int `json:"DesiredCount"`
 
 	// HealthCheckGracePeriodSeconds AWS CloudFormation Property
 	// Required: false
